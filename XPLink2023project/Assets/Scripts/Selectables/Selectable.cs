6715892ca49e3c035a935fed7e8156d259a2a75e
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2023-03-17 23:14:11 +0100

    big code push, starting work on building towers

---
+++ XPLink2023project/Assets/Scripts/Selectables/Selectable.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer highlighter;
+    [Header("State Sprites")]
+    [SerializeField] private Sprite highlightSprite;
+    [SerializeField] private Sprite selectedSprite;
+
+    private Sprite baseSprite;
+    private bool selected = false;
+
+    private void Start()
+    {
+        baseSprite = highlighter.sprite;
+    }
+
+    public virtual void BeginHover()
+    {
+        if (!selected) {
+            highlighter.sprite = highlightSprite;
+        }
+        ClickManager.instance.SetHover(this);
+    }
+    public virtual void EndHover()
+    {
+        if (!selected) {
+            highlighter.sprite = baseSprite;
+        }
+        ClickManager.instance.SetHover(null);
+    }
+
+    public virtual void BeginSelect()
+    {
+        selected = true;
+        highlighter.sprite = selectedSprite;
+        ClickManager.instance.SetSelected(this);
+    }
+    public virtual void EndSelect()
+    {
+        selected = false;
+        highlighter.sprite = baseSprite;
+    }
+
+    //============= Detect Mouse =============
+    private void OnMouseEnter()
+    {
+        BeginHover();
+    }
+
+    private void OnMouseExit()
+    {
+        EndHover();
+    }
+}
