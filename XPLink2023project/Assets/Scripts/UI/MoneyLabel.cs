be75b696441b104d77308612d06cb5f3a71acbdf
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2023-03-18 14:27:33 +0100

    Added info labels

---
+++ XPLink2023project/Assets/Scripts/UI/MoneyLabel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyLabel : MonoBehaviour
+{
+    private TMP_Text label;
+
+    private void Start()
+    {
+        label = GetComponent<TMP_Text>();
+        MoneyManager.instance.onMoneyChanged += UpdateLabel;
+        UpdateLabel(MoneyManager.instance.GetCurrentMoney());
+    }
+
+    private void UpdateLabel(int money)
+    {
+        label.text = "$" + money;
+    }
+
+    private void OnDestroy()
+    {
+        MoneyManager.instance.onMoneyChanged -= UpdateLabel;
+    }
+}
